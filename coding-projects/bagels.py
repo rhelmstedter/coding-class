50d2deb5c98ef729530488804e48c81d4669fddf
Russell Helmstedter <rhelmstedter@gmail.com>
2024-08-18 10:39:46 -0700

    updating lessons

--- coding-projects/bagels.py
+++ coding-projects/bagels.py
@@ -4,24 +4,58 @@ import random
 
 NUM_DIGITS = 3
 MAX_GUESSES = 3
+INSTRUCTIONS = f"""Instructions:\n
+I am thinking of a {NUM_DIGITS}-digit number with no repeated digits.
+Try to guess what it is. I will give you clues about your guess:
+When I say:    That means:
+  Pico           One digit is correct but in the wrong position.
+  Fermi          One digit is correct and in the right position.
+  Bagels         No digit is correct.
 
+For example, if the secret number was 248 and your guess was 843, the
+clues would be Fermi Pico.\n"""
+
+def generate_secret_num() -> str:
+    """Return a string made up of NUM_DIGITS unique random digits."""
+    numbers = list("0123456789")  # Create a list of digits 0 to 9.
+    random.shuffle(numbers)  # Shuffle them into random order.
+
+    # Get the first NUM_DIGITS digits in the list for the secret number:
+    return "".join(numbers[:3])
+
+def get_guess() -> str:
+    """Get the clue from the user."""
+    while True:
+        guess = input("> ")
+        if len(guess) != NUM_DIGITS or not guess.isdecimal():
+            print("Enter a valid guess.")
+            continue
+        else:
+            break
+    return guess
+
+def get_clues(guess, secret_num) -> str:
+    """Calculate the clues."""
+    if guess == secret_num:
+        return "You got it!"
+    clues = []
+    for guess_digit, secret_digit in zip(guess, secret_num):
+        if guess_digit == secret_digit:
+            # A correct digit is in the correct place.
+            clues.append("Fermi")
+        elif guess_digit in secret_num:
+            # A correct digit is in the incorrect place.
+            clues.append("Pico")
+    if len(clues) == 0:
+        return "Bagels"  # There are no correct digits at all.
+    else:
+        clues.sort()
+        return " ".join(clues)
 
 def main():
     """Store the logic of the game loop."""
     # Print the instructions outside the game loop.
-    print(
-        f"""Instructions:\n
-I am thinking of a {NUM_DIGITS}-digit number with no repeated digits.
-Try to guess what it is. I will give you clues about your guess:
-When I say:    That means:
-  Pico         One digit is correct but in the wrong position.
-  Fermi        One digit is correct and in the right position.
-  Bagels       No digit is correct.
-
-For example, if the secret number was 248 and your guess was 843, the
-clues would be Fermi Pico.\n"""
-    )
-
+    print(INSTRUCTIONS)
     while True:  # Main game loop.
         # Create secret number
         secret_num = generate_secret_num()
@@ -51,47 +85,6 @@ clues would be Fermi Pico.\n"""
         if not input("> ").lower().startswith("y"):
             break
     print("Thanks for playing!")
-
-
-def generate_secret_num() -> str:
-    """Return a string made up of NUM_DIGITS unique random digits."""
-    numbers = list("0123456789")  # Create a list of digits 0 to 9.
-    random.shuffle(numbers)  # Shuffle them into random order.
-
-    # Get the first NUM_DIGITS digits in the list for the secret number:
-    return "".join(numbers[:3])
-
-
-def get_guess() -> str:
-    """Get the clue from the user."""
-    while True:
-        guess = input("> ")
-        if len(guess) != NUM_DIGITS or not guess.isdecimal():
-            print("Enter a valid guess.")
-            continue
-        else:
-            break
-    return guess
-
-
-def get_clues(guess, secret_num) -> str: parameters or arguments
-    """Calculate the clues."""
-    if guess == secret_num:
-        return "You got it!"
-    clues = []
-    for guess_digit, secret_digit in zip(guess, secret_num):
-        if guess_digit == secret_digit:
-            # A correct digit is in the correct place.
-            clues.append("Fermi")
-        elif guess_digit in secret_num:
-            # A correct digit is in the incorrect place.
-            clues.append("Pico")
-    if len(clues) == 0:
-        return "Bagels"  # There are no correct digits at all.
-    else:
-        clues.sort()
-        return " ".join(clues)
-
 
 # If the program is run (instead of imported), run the game:
 if __name__ == "__main__":
